b790cd286a455abe0d3dce6971e6cfef7c77f194
root <root@Pich.>
2025-03-15 19:58:54 +0700

    fix

--- soal_1/poppo_siroyo.sh
+++ soal_1/poppo_siroyo.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
 
-awk 'BEGIN {FS=","}\
+if [ ! -f reading_data.csv ]; then
+    wget -O reading_data.csv "https://drive.usercontent.google.com/u/0/uc?id=1l8fsj5LZLwXBlHaqhfJVjz_T0p7EJjqV&export=download"
+fi
+
+echo "Pilih opsi yang ingin ditampilkan:"
+echo "1. Jumlah buku yang dibaca oleh Chris Hemsworth"
+echo "2. Rata-rata durasi membaca dengan Tablet"
+echo "3. Pembaca dengan rating tertinggi"
+echo "4. Genre paling populer di Asia setelah 2023"
+read -p "Masukkan pilihan (1-4): " pilihan
+
+if ! [[ "$pilihan" =~ ^[1-4]$ ]]; then
+    echo "Pilihan tidak valid."
+    exit 1
+fi
+
+if [ "$pilihan" -eq 1 ]; then
+   awk 'BEGIN {FS=","}\
 	$0 ~/Chris Hemsworth/ {++n }\
 	END {if(n>0)\
 		{print "Chris Hemsworth membaca", n, "buku"}\
 	else\
 		{print "Chris Hemsworth tidak membaca buku"}\
 	}'\
-	 /home/nadiakusuma1001/sisop1_1/reading_data.csv
+	reading_data.csv
 
-awk 'BEGIN {FS=","; sum=0; n=0}\
+elif [ "$pilihan" -eq 2 ]; then
+   awk 'BEGIN {FS=","; sum=0; n=0}\
 	 $8 ~ /Tablet/ {sum += $6; n++}\
 	 END {if (n>0){\
 		printf "Rata-rata durasi baca dengan Tablet: %.2f menit \n", sum/n}\
 	else {\
 		print "Tidak ada yang membaca menggunakan Tablet"}\
 	}'\
-	 /home/nadiakusuma1001/sisop1_1/reading_data.csv
-awk 'BEGIN {FS=","; m=0}\
+	reading_data.csv 
+
+elif [ "$pilihan" -eq 3 ]; then
+   awk 'BEGIN {FS=","; m=0}\
 	NR > 1 \
 	{if($7 > m) {m = $7; b = $3; n = $2}}\
 	END {print "Pembaca dengan rating tertinggi adalah", n, "berjudul", b, "dengan rating", m}'\
-	/home/nadiakusuma1001/sisop1_1/reading_data.csv
-awk 'BEGIN {FS=","; prev=0}\
+	reading_data.csv
+
+elif [ "$pilihan" -eq 4 ]; then
+   awk 'BEGIN {FS=","; prev=0}\
 	$9 ~ /Asia/ && $5 > "2023-12-31"\
 	{count[$4]++; for(genre in count){\
 		if(count[genre] > prev){\
 			prev = count[genre]; g = genre}\
 	}\
 	} END {print "Genre paling populer di Asia setelah 2023 adalah", g, "dengan buku sebanyak", prev}'\
-	/home/nadiakusuma1001/sisop1_1/reading_data.csv
+	reading_data.csv  
+fi
